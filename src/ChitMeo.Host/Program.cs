ee51414ef56baaff242194f1eb85a3347bdb90a8
dev <dev@chitmeo.com>
2026-03-11 16:19:18 +0700

    Wrap application startup in try-catch for improved error handling

--- src/ChitMeo.Host/Program.cs
+++ src/ChitMeo.Host/Program.cs
@@ -1,20 +1,15 @@
 using ChitMeo.Host.Extensions;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.ConfigureWebApplication();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.ConfigureWebApplication();
 
-WebApplication app = builder.Build();
-await app.ConfigureRequestPipelineAsync();
-
-
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Add services to the container.
-
-// var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-
-// app.UseHttpsRedirection();
-
-// app.Run();
+    var app = builder.Build();
+    await app.ConfigureRequestPipelineAsync();
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine(ex);
+    throw;
+}
